9f2af21a8d1e6d0cd3a0dd4363cdb8ea41b58cd3
lmanuelm23 <79720384+lmanuelm23@users.noreply.github.com>
2021-10-11 20:22:27 -0500

    Cambios Luis

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,17 +5,17 @@
 	<!-- Required meta tags -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="icon" href="{{asset('img/favicon.png')}}" type="image/png">
+	<link rel="icon" href="img/favicon.png" type="image/png">
 	<title>LMDev Portfolio</title>
 	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
-	<link rel="stylesheet" href="{{asset('vendors/linericon/style.css')}}">
-	<link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
-	<link rel="stylesheet" href="{{asset('vendors/owl-carousel/owl.carousel.min.css')}}">
-	<link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
-	<link rel="stylesheet" href="{{asset('vendors/nice-select/css/nice-select.css')}}">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="vendors/linericon/style.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
 	<!-- main css -->
-	<link rel="stylesheet" href="{{asset('css/style.css')}}">
+	<link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
